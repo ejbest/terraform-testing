d8c8381f3d492c6497c3446205fd5158d6d238b5
Erich (EJ) Best <ejbest@gmail.com>
2024-11-26 11:05:37 -0500

    documenting and testing

--- terraform2hours/version3/main.tf
+++ terraform2hours/version3/main.tf
@@ -8,6 +8,7 @@ provider "aws" {
   region = local.ejb_region
 }
 
+# 1. Create VPC
 resource "aws_vpc" "ejb-prod-vpc" {
   cidr_block = local.ejb_cidr_block
   tags = {
@@ -15,36 +16,35 @@ resource "aws_vpc" "ejb-prod-vpc" {
   }
 }
 
+# generate private and corresponding public key
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
-  rsa_bits  = 2048
+  rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "generated_key" {
+
+# resource "null_resource" "ej-priv-key" 
+resource "null_resource" "ej-priv-key" {
+  provisioner "local-exec" {
+    command = <<EOT
+    echo "${tls_private_key.ssh_key.private_key_pem}" > ej.pem && chmod 0600 ej.pem
+    EOT
+  }
+}
+
+
+resource "aws_key_pair" "ej_key" {
   key_name   = local.ejb_key_name
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-
-# resource "null_resource" "ej-priv-key" {
-#   provisioner "local-exec" {
-#     command = "echo ${tls_private_key.ssh_key.private_key_pem}' > ej"
-#   }
-# }
-
-# resource "null_resource" "ej-priv-key" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       echo "${tls_private_key.ssh_key.private_key_pem} > ej && chmod 0600 ej"
-#     EOT
-#   }
-# }
-
+# 9. Create the WebServer
 resource "aws_instance" "ejb-webserver" {
   ami               = local.ejb_ami_id
   instance_type     = local.ejb_instance_type
   availability_zone = local.ejb_availability_zone
-  key_name          = aws_key_pair.generated_key.key_name
+  key_name          = aws_key_pair.ej_key.key_name# Use the public key for AWS EC2
+ 
 
   network_interface {
     device_index         = 0
